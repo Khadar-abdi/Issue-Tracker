777a62728f4b2762603c9ea038362b678fb0b25f
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2025-01-05 17:42:55 +0300

    impeliment client side validation

--- app/api/issue/route.ts
+++ app/api/issue/route.ts
@@ -1,18 +1,16 @@
 import prisma from "@/prisma/client";
+import { IssueSchema } from "@/Schemas/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+ 
 
-const IssueSchema = z.object({
-    title: z.string().min(1).max(100),
-    description: z.string().min(1).max(1000),
-});
+
 
 export async function POST( request:NextRequest){
     const body = await request.json();
     const validation =  IssueSchema.safeParse(body);
 
     if(!validation.success){
-        return NextResponse.json({error: validation.error.errors}, {status: 400});
+        return NextResponse.json({error: validation.error.format()}, {status: 400});
     }
 
     const newIssue = await prisma.issue.create({
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,33 +1,45 @@
 'use client'
-import { Button, TextField } from '@radix-ui/themes'
-import React from 'react'
-// import SimpleMdeReact from 'react-simplemde-editor';
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import React, { useState } from 'react'
 import "easymde/dist/easymde.min.css";
 import dynamic from 'next/dynamic';
 import { Controller, useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { LoaderIcon } from 'lucide-react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import  {z} from 'zod';
+import { IssueSchema } from '@/Schemas/validationSchema';
 
 const SimpleMdeReact = dynamic(() => import('react-simplemde-editor'), { ssr: false });
 
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
 
+type IssueForm = z.infer<typeof IssueSchema>;
+
+
+ 
 
 const page = () => {
 
     const router = useRouter();
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const [ error, setError]=useState('');
+    const { register, control, handleSubmit, formState:{ errors, isSubmitting} } = useForm<IssueForm>({
+        resolver: zodResolver(IssueSchema)
+    });
 
 
     const onSubmit = async (data: IssueForm ) => {
 
-       const response =await axios.post('/api/issue', data)
-       if(response.status === 201){
-           router.push('/issues')
+        try {
+            
+            const response =await axios.post('/api/issue', data)
+            if(response.status === 201){
+                router.push('/issues')
+            
+        }
+        } catch (error) {
+           setError('An unexpected error occured, please try again.')
        } 
 
 
@@ -37,12 +49,19 @@ const page = () => {
 
 
     return (
-        <form className='flex max-w-xl flex-col   space-y-5'
+            <div className='max-w-xl space-y-5'> 
+                  <h1 className=' text-slate-400 text-xl my-3 font-sans font-semibold' > Create Issue </h1>
+
+                {error &&  <Callout.Text color='red' className='text-[#f76c6c] text-sm '> {error}</Callout.Text> }
+
+        <form className='flex  flex-col   space-y-5'
+
             onSubmit={handleSubmit(onSubmit)} >
             {/* creating Issues */}
-            <h1 className=' text-slate-400 text-xl font-sans font-semibold' > Create Issue </h1>
+          
 
             <TextField.Root className='text-slate-500 font-sans ' placeholder='Issue Title' {...register('title')} />
+            { errors.title && <p className='text-red-500'>{errors.title?.message} </p> }
             <Controller
                 name='description'
                 control={control}
@@ -50,11 +69,13 @@ const page = () => {
                     <SimpleMdeReact {...field} placeholder='Issue Description' className='max-w-full' />
                 )}
             />
+             { errors.description &&  <p  className='text-red-500'>{errors.description?.message} </p> }
             {/* <SimpleMdeReact placeholder='Issue Description' className='max-w-full'  /> */}
 
-            <Button type='submit'>Submit New Issue</Button>
+            <Button disabled={isSubmitting} type='submit'  >Submit New Issue {isSubmitting && <LoaderIcon />} </Button>
 
         </form>
+        </div>
     )
 }
 
