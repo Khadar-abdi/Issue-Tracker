75c3eb48e076ae5c7ce56953e6efb9fc79aa166e
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2025-01-05 11:35:03 +0300

    create New Issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,18 @@
+import { Button, TextArea, TextField } from '@radix-ui/themes'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex w-2/5 flex-col space-y-5' >
+        {/* creating Issues */}
+        <h1 className=' text-slate-400 text-xl font-sans font-semibold' > Create Issue </h1>
+
+        <TextField.Root placeholder='Issue Title' className='w-full' />
+        <TextArea placeholder=' Your Description' />
+
+        <Button type='submit'>Submit New Issue</Button>
+           
+    </div>
   )
 }
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const IssuePage = () => {
   return (
-    <div> 
-      <Button>  New Issue </Button>
+    <div > 
+     <Link href={'/issues/new'}  > <Button>  New Issue </Button> </Link>
        
                 
     </div>
