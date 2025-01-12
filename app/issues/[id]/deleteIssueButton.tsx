d590c0d36c0a6ef523f6a2d4cc8b37cd0de78956
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2025-01-12 12:56:03 +0300

    show a confirmation dailog

--- app/issues/[id]/deleteIssueButton.tsx
+++ app/issues/[id]/deleteIssueButton.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+
 import { Delete } from 'lucide-react'
  
 import Link from 'next/link'
@@ -8,7 +10,34 @@ import React from 'react'
 
 const DeleteIssueButton = ({issueId}:  { issueId: number} ) => {
   return (
-      <Link href={`/issues/${issueId}/edit`  } ><Button color='gray' className='flex flex-row space-x-5 text-sm '  > Delete <Delete/></Button></Link>
+
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+     <Button color='gray' variant='soft' className='flex flex-row space-x-5 text-sm '  > Delete </Button> 
+
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>
+          Confirm Deletion
+        </AlertDialog.Title>
+        <AlertDialog.Description>
+          Are You sure to Delete Issue
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+			<AlertDialog.Cancel>
+				<Button variant="soft" color="gray">
+					Cancel
+				</Button>
+			</AlertDialog.Cancel>
+			<AlertDialog.Action>
+				<Button variant="solid" >
+					Delete Issue
+				</Button>
+			</AlertDialog.Action>
+		</Flex>
+      </AlertDialog.Content>
+
+    </AlertDialog.Root>
   )
 }
 
