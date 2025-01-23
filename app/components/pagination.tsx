2ae418013830535abaea8ea267decab1aabab882
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2025-01-23 09:38:36 +0300

    impeliment paginations

--- app/components/pagination.tsx
+++ app/components/pagination.tsx
@@ -1,5 +1,7 @@
+'use client'
 import { Button, Flex, Text } from '@radix-ui/themes';
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react'
 
 interface props{
@@ -9,22 +11,32 @@ interface props{
 }
 
 const Pagination = ({itemCount, currentPage, pageSize  }: props) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
     const pageCount = Math.ceil(itemCount/pageSize)
     if(pageCount <= 1) return null
-    console.log(pageCount)
+
+    const ChangePage =(page: number)=>{
+        const params = new URLSearchParams(searchParams);
+        params.set('page', page.toString());
+        const query = params.toString() ? `?${params.toString()}` : ''
+        router.push(`/${query}`)
+    }
+
   return (
     <Flex gap='2' align='center' justify='center'>
         <Text size='2' className='text-slate-600 font-sans'> page {currentPage}  of {pageCount}</Text>
-        <Button  variant='soft' disabled={currentPage === 1}>
+        <Button  variant='soft' disabled={currentPage === 1} onClick={()=>ChangePage(1)}>
         <ChevronsLeft />
         </Button>
-        <Button  variant='soft' disabled={currentPage === 1}>
+        <Button  variant='soft' disabled={currentPage === 1} onClick={()=>ChangePage(currentPage - 1)}>
         <ChevronLeft />
         </Button>
-        <Button  variant='soft' disabled={currentPage === pageCount}>
+        <Button  variant='soft' disabled={currentPage === pageCount} onClick={()=>ChangePage(currentPage + 1)}>
         <ChevronRight />
         </Button>
-        <Button  variant='soft' disabled={currentPage === pageCount}>
+        <Button  variant='soft' disabled={currentPage === pageCount}  onClick={()=>ChangePage(pageCount)}>
         <ChevronsRight />
         </Button>
        
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
 import Pagination from './components/pagination'
 
-export default function Home() {
+ 
+async function Home({searchParams}: {searchParams: {page: string}}) {
+  const SearchParams = await searchParams;
   return (
     <div>
-      <Pagination itemCount={100} currentPage={10} pageSize={10} />
+      <Pagination itemCount={100}  pageSize={2} currentPage={parseInt(SearchParams.page)} />
     </div>
   )
 }
+export default Home
