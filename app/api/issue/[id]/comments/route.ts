d281b1eaf11638693e7b0e8396367baa99d9cb8b
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2025-01-28 13:36:31 +0300

    handle route

--- app/api/issue/[id]/comments/route.ts
+++ app/api/issue/[id]/comments/route.ts
@@ -1,34 +1,32 @@
-import authOptions from "@/app/auth/authOptions";
-import prisma from "@/prisma/client";
-import { CommentSchema, IssueSchema } from "@/Schemas/validationSchema";
-import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import prisma from '@/prisma/client';
+import { CommentSchema } from '@/Schemas/validationSchema';
+import authOptions from '@/app/auth/authOptions';
 
-export async function POST(request: NextRequest,{params}: {params: { id: string}}) {
+export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
     const session = await getServerSession(authOptions);
     if (!session) return NextResponse.json({}, { status: 401 });
-  
+
     const body = await request.json();
     const validation = CommentSchema.safeParse(body);
-  
+
     if (!validation.success) {
-      return NextResponse.json(
-        { error: validation.error.format() },
-        { status: 400 }
-      );
+        return NextResponse.json(
+            { error: validation.error.format() },
+            { status: 400 }
+        );
     }
 
-    console.log(params.id)
-  
+    console.log(params.id);
+
     const newComment = await prisma.comment.create({
-      data: {
-        content: validation.data.content,
-        issueId:parseInt(params.id),
-        userId: session.user.id
-        
-        
-      },
+        data: {
+            content: validation.data.content,
+            issueId: parseInt(params.id),
+            userId: session.user.id,
+        },
     });
-  
+
     return NextResponse.json(newComment, { status: 201 });
-  }
+}
