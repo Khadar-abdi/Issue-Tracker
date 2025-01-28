d8608faac66bcbc36183a639ba85cfdbdc94f20a
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2025-01-28 18:05:44 +0300

    solve route

--- app/api/issue/[id]/comments/route.ts
+++ app/api/issue/[id]/comments/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest, NextResponse} from 'next/server'
 import { getServerSession } from 'next-auth';
 import prisma from '@/prisma/client';
 import { CommentSchema } from '@/Schemas/validationSchema';
