e40703cd32339fe684cbf760c5b411d5a88df0c5
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2025-01-28 20:25:36 +0300

    comment route

--- app/api/issue/[id]/comments/route.ts
+++ app/api/issue/[id]/comments/route.ts
@@ -1,38 +1,35 @@
 import authOptions from "@/app/auth/authOptions";
 import prisma from "@/prisma/client";
-import { CommentSchema, IssueSchema } from "@/Schemas/validationSchema";
+import { CommentSchema } from "@/Schemas/validationSchema";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-interface props {
-    params: { id: string}
-    }
-
-export async function POST(request: NextRequest,{params}:props ) {
-    const session = await getServerSession(authOptions);
-    if (!session) return NextResponse.json({}, { status: 401 });
-  
-    const body = await request.json();
-    const validation = CommentSchema.safeParse(body);
-  
-    if (!validation.success) {
-      return NextResponse.json(
-        { error: validation.error.format() },
-        { status: 400 }
-      );
-    }
-
- 
-  
-    const newComment = await prisma.comment.create({
-      data: {
-        content: validation.data.content,
-        issueId:parseInt(params.id),
-        userId: session.user.id
-        
-        
-      },
-    });
-  
-    return NextResponse.json(newComment, { status: 201 });
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { id: string } } // Fixing the type here
+) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({}, { status: 401 });
   }
+
+  const body = await request.json();
+  const validation = CommentSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: validation.error.format() },
+      { status: 400 }
+    );
+  }
+
+  const newComment = await prisma.comment.create({
+    data: {
+      content: validation.data.content,
+      issueId: parseInt(params.id, 10), // Ensure `id` is converted to a number
+      userId: session.user.id,
+    },
+  });
+
+  return NextResponse.json(newComment, { status: 201 });
+}
